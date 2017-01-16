60fb39e8bdce8e966938461793721059ea086092
ikkkkkk <ikue.yawat@gmail.com>
2017-01-16 11:25:19 +0900

    final ver by yawata

---
+++ chi.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python                                                               
+# -*- coding: utf-8 -*-                                                         
+import tweepy
+import codecs
+import re
+
+sfc_list = []
+non_list = []
+
+all_list = []
+
+sfc_dic = {}
+non_dic = {}
+
+sfc_chi_dic = {}
+non_chi_dic = {}
+
+
+for line in codecs.open("dict_non.txt","r","euc-jp"):
+    line = line.rstrip()
+    lis = line.split(" ")
+    non_dic[lis[0]] = int(lis[1])
+    if lis[0] not in all_list:
+        all_list.append(lis[0])
+
+for line in codecs.open("dict_sfc.txt","r","euc-jp"):
+    line = line.rstrip()
+    lis = line.split(" ")
+    sfc_dic[lis[0]] = int(lis[1])
+    if lis[0] not in all_list:
+        all_list.append(lis[0])
+
+for w in all_list:
+
+    x = 0
+    y = 0
+
+    if w in non_dic and w in sfc_dic:
+        x = sfc_dic[w]
+        y = non_dic[w]
+    elif w in non_dic and w not in sfc_dic:
+        y = non_dic[w]
+    elif w not in non_dic and w in sfc_dic:
+        x = sfc_dic[w]
+
+    z = x + y
+    a = float(z/2.0)
+    b = float(z/2.0)
+
+    chi_sv_sfc = float((x - a) **2 /a )
+    chi_sv_non = float((y - b) **2 /b )
+    chi_square_value = float(chi_sv_sfc + chi_sv_non)
+    if chi_square_value > 3 and z > 4:
+        if re.search(ur"^[ぁ-んァ-ン]$",w):
+            pass
+        else:
+            print ("%d %d %f %f %f %s" % (x, y, chi_sv_sfc, chi_sv_             non, chi_square_value, w))
+            if x > y:
+                sfc_chi_dic[w] = chi_square_value
+            elif x < y:
+                non_chi_dic[w] = chi_square_value
+            else:
+                pass
+
+fp = codecs.open('/home/yawata/wts/final/sfc_rank.txt','w', 'euc-jp             ')
+sfc_rank = []
+non_rank = []
+print "sfc"
+for key in sorted(sfc_chi_dic.items(),key=lambda x:x[1],reverse=Tru             e):
+    sfc_rank.append(key[0])
+
+#    print key[0],key[1]
+for i in sfc_rank[0:3]:
+    fp.write("sfc: %s\n" % (i))
+
+print "non"
+for key in sorted(non_chi_dic.items(),key=lambda x:x[1],reverse=Tru             e):
+    non_rank.append(key[0])
+#    print key[0],key[1]
+for i in non_rank[0:3]:
+    fp.write("non: %s\n" % (i))
+
+
+
+
+
+
+#for sfc in sfc_list:
+    #print "{0}: {1}".format(sfc[0].encode('utf-8'), sfc[1])
+#    sfc_word = sfc[0]
+#    sfc_count = int(sfc[1])
+#    for non in non_list:
+#        non_word = non[0]
+#        non_count = int(non[1])
+
+#        if sfc_word == non_word:
+#            if sfc_count > non_count:
+#                fp.write("sfc: %s\n" % (sfc_word))
+#            else:
+#                fp.write("non: %s\n" % (non_word))
+
+
--- hindo_sfc.py
+++ hindo_sfc.py
@@ -6,18 +6,19 @@ import re
 
 dict_sfc = {}
 
-for line in codecs.open('/home/t16298sk/wts3/tweet_sfc.txt.chasen','r','euc-jp'):
+for line in codecs.open('/home/yawata/wts/final/tweet_sfc.txt.chasen','r','euc-jp'):
     line = line.rstrip('\r\n')
     if line == "EOS":
         pass
     else:
         lis = line.split("\t")
-        if re.search(ur'名詞',lis[3]):
+        if re.search(ur'^(名詞-一般|名詞-サ変接続)',lis[3]):
             if lis[0] in dict_sfc:
                 dict_sfc[lis[0]] += 1
             else:
                 dict_sfc[lis[0]] = 1
 
-fp = codecs.open('/home/t16298sk/wts3/dict_sfc.txt', 'w', 'euc-jp')
+fp = codecs.open('/home/yawata/wts/final/dict_sfc.txt', 'w', 'euc-jp')
 for x in sorted(dict_sfc.items(), key=lambda x:x[1],reverse=True):
-    print x[0], x[1]
+#print x[0], x[1]
+    fp.write("%s %s\n" % (x[0],x[1]))
