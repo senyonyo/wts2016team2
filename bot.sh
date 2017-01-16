231fab4bbc4a527496caeb8eb9eb8089ca83169d
ikkkkkk <ikue.yawat@gmail.com>
2017-01-16 11:32:39 +0900

    final ver

--- bot.sh
+++ bot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-WD=/home/t16298sk/wts3
+#WD=/home/t16298sk/wts3
+WD=/home/yawata/wts/final
 python ${WD}/crawl_sfc.py 300 ${WD}/tweet_sfc.txt
 chasen < ${WD}/tweet_sfc.txt > ${WD}/tweet_sfc.txt.chasen
 python ${WD}/crawl_hiyoshi.py 300 ${WD}/tweet_non.txt
 chasen < ${WD}/tweet_non.txt > ${WD}/tweet_non.txt.chasen
 python ${WD}/hindo_sfc.py
 python ${WD}/hindo_non.py
-
-python ${WD}/send.py
-
+python ${WD}/chi.py
+python ${WD}/send.py%
